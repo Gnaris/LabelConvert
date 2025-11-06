3929bef95ea72fec7629c81ea0ff68bae0109f17
Gnaris <chen.boris@hotmail.fr>
2025-11-07 00:36:27 +0100

    2

--- fonction.js
+++ fonction.js
@@ -3,7 +3,7 @@ const { PDFDocument } = require("pdf-lib");
 const fs = require("fs")
 const { PDFParse } = require('pdf-parse');
 
-async function convertLabel(url, message) {
+async function convertLabel(url, message, iteration) {
 
     const response = await axios.get(url, { responseType: "arraybuffer" });
 
@@ -11,28 +11,34 @@ async function convertLabel(url, message) {
     const newPdf = await PDFDocument.create();
 
     const [firstPage] = await newPdf.copyPages(pdfDoc, [0]);
-
     const { width, height } = firstPage.getSize();
     firstPage.setCropBox(0, 0, width / 2, height);
-
     newPdf.addPage(firstPage);
 
     const newPdfBytes = await newPdf.save();
 
-    const filePath = "./bordereau_converti.pdf";
+    // l'itération c'est pour éviter que le gars envois plusieurs bordereau d'un coup et que ça créer un conflit de fichier
+    const filePath = "./bordereau_converti" + iteration + ".pdf";
     fs.writeFileSync(filePath, newPdfBytes);
 
     const parser = new PDFParse({ url: url });
     const result = await parser.getText();
-    const resultSplit = result.text.split("\n")
-    const destinataire = resultSplit[23]
-    const suivi = resultSplit[33]
+    let resultSplit = result.text.split("\n")
+    let destinaire = resultSplit[23];
+    let suivi;
+    resultSplit.forEach(text => {
+        if(text.replace(/\s+/g, '').match(/\b8[A-Z]\d{11,}\b/g)) // enlève tout les espaces puis check si ça correspond au format du numéro suivi
+        {
+            suivi = text.replace(/\s+/g, '')
+        }
+    })
 
     await message.reply({
-        content: destinataire + " - " + suivi,
+        content: destinaire + " - " + suivi,
         files: [filePath],
     });
-    fs.unlinkSync(filePath);
+
+    fs.unlinkSync(filePath)
 }
 
 module.exports = { convertLabel }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const { convertLabel } = require('./fonction');
-const token = 'MTQzNTkzOTIxNzAwMjk5MTY1Ng.GbBqif.adOXGiy13pqo2gj_Sdf8dfJGkIXbSscHlBGwZw';
+const token = 'MTQzNTkzOTIxNzAwMjk5MTY1Ng.GQU3va.IDS-ard9szLH2vWWn5gtBchpjIG65NS-ktR8ns';
 const clientId = '1435939217002991656';
 const guildId = '1435940865397755990';
 
@@ -8,13 +8,23 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 
 client.on('messageCreate', async message => {
-  if(message.author.bot) return;
-  message.attachments.forEach(attachment => {
-    if(attachment.size < 0 || attachment.contentType != 'application/pdf' || !attachment.name.endsWith(".pdf")) return;
-    convertLabel(attachment.url, message)
-  })
-
+  console.log(message.content)
+  if (message.author.bot) return;
+  let i = 0;
+  for (const attachment of message.attachments.values()) {
+    if (attachment.size > 0 || attachment.contentType == 'application/pdf' || attachment.name.endsWith(".pdf"))
+    {
+      await convertLabel(attachment.url, message, i);
+      i++
+      if(i >= message.attachments.size)
+      {
+        message.delete()
+      }
+    }
+  }
 })
 
-client.once('ready', () => console.log(`Connecté en tant que ${client.user.tag}`));
+client.once('ready', () => {
+  console.log(`Connecté en tant que ${client.user.tag}`)
+});
 client.login(token);
