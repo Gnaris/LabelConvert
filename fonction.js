98daa5818b8aff48c1291f388a5b51376a7ed161
Gnaris <chen.boris@hotmail.fr>
2025-11-07 19:04:56 +0100

    fsdz

--- fonction.js
+++ fonction.js
@@ -1,9 +1,9 @@
 const axios = require("axios")
-const { PDFDocument } = require("pdf-lib");
+const { PDFDocument, rgb } = require("pdf-lib");
 const fs = require("fs")
 const { PDFParse } = require('pdf-parse');
 
-async function convertLabel(url, message, iteration) {
+async function convertLabel(url, message, iteration, quantity = "") {
 
     const response = await axios.get(url, { responseType: "arraybuffer" });
 
@@ -11,10 +11,18 @@ async function convertLabel(url, message, iteration) {
     const newPdf = await PDFDocument.create();
 
     const [firstPage] = await newPdf.copyPages(pdfDoc, [0]);
-    firstPage.setCropBox(55.5, 163.5, 286, 342); // gauche, bas, droite, haut
+    firstPage.setCropBox(45, 155, 307, 360); // gauche, bas, droite, haut
     newPdf.addPage(firstPage);
 
-    const newPdfBytes = await newPdf.save();
+    newPdf.getPages()[0].drawText(quantity, {
+        x: firstPage.getCropBox().width - 210,
+        y: firstPage.getCropBox().height + 50,
+        size: 10,
+        color: rgb(0, 0, 0),
+        opacity: 1,
+    })
+
+    const newPdfBytes = await newPdf.save()
 
     // l'itération c'est pour éviter que le gars envois plusieurs bordereau d'un coup et que ça créer un conflit de fichier
     const filePath = "./bordereau_converti" + iteration + ".pdf";
@@ -33,11 +41,69 @@ async function convertLabel(url, message, iteration) {
     })
 
     await message.reply({
-        content: destinaire + " - " + suivi,
+        content: destinaire + "\n" + suivi + (quantity && "\n" + "# --> [ " + quantity + " ] <--"),
         files: [filePath],
     });
 
     fs.unlinkSync(filePath)
 }
 
-module.exports = { convertLabel }
+async function regroupLabel(interaction)
+{
+    const messages = await interaction.channel.messages.fetch({ limit: 100 })
+    const messagesFilter = []
+    messages.forEach((message) => {
+        if(message.attachments.size > 0 && !message.content.includes("bordereaux regroupés"))
+        {
+            message.attachments.forEach((attachment) => {
+                if(attachment.name.endsWith('.pdf') && attachment.contentType == 'application/pdf')
+                {
+                    messagesFilter.push(message)
+                }
+            })
+        }
+    })
+
+    const pdfUrl = []
+    messagesFilter.forEach((message) => {
+        message.attachments.forEach((attachment) => {
+            pdfUrl.push(attachment.url)
+        })
+    })
+    const request = pdfUrl.map(url => axios.get(url, {responseType : "arraybuffer"}))
+    const responses = await axios.all(request)
+    const pdfGrouped = await PDFDocument.create()
+    for (const response of responses) {
+        const pdf = await PDFDocument.load(response.data);
+        const pages = await pdfGrouped.copyPages(pdf, pdf.getPageIndices());
+        pages.forEach(page => pdfGrouped.addPage(page));
+    }
+    const pdfGroupedBytes = await pdfGrouped.save()
+    const filePath = "./" + interaction.channel.name + "_groupés.pdf"
+    fs.writeFileSync(filePath , pdfGroupedBytes)
+    await interaction.editReply({
+        content : "# **" + pdfGrouped.getPages().length + " bordereaux regroupés**",
+        files : [filePath]
+    })
+    fs.unlinkSync(filePath)
+}
+
+async function countLabel(interaction)
+{
+    let count = 0;
+    const messages = await interaction.channel.messages.fetch({ limit: 100 })
+    messages.forEach((message) => {
+        if(message.attachments.size > 0 && !message.content.includes("bordereaux regroupés"))
+        {
+            message.attachments.forEach((attachment) => {
+                if(attachment.name.endsWith('.pdf') && attachment.contentType == 'application/pdf')
+                {
+                    count++
+                }
+            })
+        }
+    })   
+    interaction.reply("# Total label : " + count)                 
+}
+
+module.exports = { convertLabel, regroupLabel, countLabel }
--- index.js
+++ index.js
@@ -1,25 +1,60 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { convertLabel } = require('./fonction');
+const { Client, GatewayIntentBits, SlashCommandBuilder, Collection, REST, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { convertLabel, regroupLabel, countLabel } = require('./fonction');
 const token = 'MTQzNTkzOTIxNzAwMjk5MTY1Ng.GQU3va.IDS-ard9szLH2vWWn5gtBchpjIG65NS-ktR8ns';
 const clientId = '1435939217002991656';
-const guildId = '1435940865397755990';
+const guildIds = [
+  "1435940865397755990",
+  "1435294428930768958"
+]
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+try {
+  const commands = [
+    new SlashCommandBuilder().setName("group").setDescription("Regroupe les PDF de ce channel en un seul fichier PDF").toJSON(),
+    new SlashCommandBuilder().setName("count").setDescription("Il va compter combien de bordereaux il y a dans ce channel").toJSON(),
+  ]
+
+  const rest = new REST({ version: "10" }).setToken(token)
+  for (const guildId of guildIds) {
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  }
+} catch (err) {
+  console.log(err)
+}
+
+client.on('interactionCreate', async interaction => {
+  try {
+    if (interaction.isChatInputCommand()) {
+      if (interaction.commandName == "group") {
+        interaction.deferReply()
+        await regroupLabel(interaction)
+      }
+      if (interaction.commandName == "count") {
+        await countLabel(interaction)
+      }
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  return;
+})
 
 
 client.on('messageCreate', async message => {
   if (message.author.bot) return;
-  let i = 0;
-  for (const attachment of message.attachments.values()) {
-    if (attachment.size > 0 || attachment.contentType == 'application/pdf' || attachment.name.endsWith(".pdf"))
-    {
-      await convertLabel(attachment.url, message, i);
-      i++
-      if(i >= message.attachments.size)
-      {
-        message.delete()
+  try {
+    let i = 0;
+    for (const attachment of message.attachments.values()) {
+      if (attachment.size > 0 || attachment.contentType == 'application/pdf' || attachment.name.endsWith(".pdf")) {
+        await convertLabel(attachment.url, message, i, message.content);
+        i++
+        if (i >= message.attachments.size) {
+          message.delete()
+        }
       }
     }
+  } catch (err) {
+    console.log(err)
   }
 })
 
